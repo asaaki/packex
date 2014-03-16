e73d92d219f2306d7ca2a5558c08d3cbc32a17d1
Christoph Grabo <chris@dinarrr.com>
2014-03-16 16:05:59 +0100

    Clean up priv/release.sh

--- priv/release.sh
+++ priv/release.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "RELEASING PROJECT"
 echo
-echo "Fetching dependencies and precompile (for mix tasks) ..."
+echo "Fetching dependencies and compile ..."
 mix do deps.get, compile
-
-echo
-echo "Compiling project ..."
-mix compile
 
 echo "Assembling and bundling ..."
 mix relex.assemble
